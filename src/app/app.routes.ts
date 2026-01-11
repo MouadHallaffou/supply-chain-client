9c7328b763d11c691c4994fdda6c13c5f3172193
Mouad Hallaffou <mouadhallaffou@gmail.com>
2026-01-11 21:24:35 +0100

    add header and sidebar components with routing configuration

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import {AppComponent} from './app.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'user-management',
+    pathMatch: 'full'
+  },
+  {
+    path: 'user-management',
+    loadChildren: () => import('./features/user-management/user-management.routes').then(m => m.USER_MANAGEMENT_ROUTES)
+  },
+  {
+    path: 'inventory',
+    loadChildren: () => import('./features/inventory/inventory.routes').then(m => m.INVENTORY_ROUTES)
+  }
+];
---
+++ src/app/shared/components/sidbare/sidbare.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidbare',
+  imports: [],
+  templateUrl: './sidbare.component.html',
+  styleUrl: './sidbare.component.css'
+})
+export class SidbareComponent {
+
+}

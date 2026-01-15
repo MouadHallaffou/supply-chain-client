c5402ce8141745ee1de4d25a13c7dd01c80d50ea
Mouad Hallaffou <mouadhallaffou@gmail.com>
2026-01-15 09:54:58 +0100

    add bill-of-material, product-list, and product-order-list components with styles, templates, and tests

---
+++ src/app/features/production/components/product-order-list/product-order-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-order-list',
+  imports: [],
+  templateUrl: './product-order-list.component.html',
+  styleUrl: './product-order-list.component.css'
+})
+export class ProductOrderListComponent {
+
+}
